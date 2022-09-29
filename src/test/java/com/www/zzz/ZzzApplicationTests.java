e01a70f02222ca57d898e214aa7683d13370175a
wilee <wilee@descente.co.kr>
2022-09-29 11:12:13 +0900

    Initial commit

---
+++ src/test/java/com/www/zzz/ZzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.zzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
